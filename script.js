c8912e3ff33bc16156f93450aa3f1de7afc68b10
danielld75 <danielld@poczta.fm>
2017-12-05 05:54:59 +0100

    Remove round brackets after var note and add new line in console.log

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ var maleNames = ["Gregory", "Misiek", "Pupil", "Henio"];
 var allNames = femaleNames.concat(maleNames);
 
 var addName = function(newName) {
-  var note = ("Próbowałeś dodać imię: \"" + newName + "\", które już istnieje w bazie.");
+  var note = "Próbowałeś dodać imię: \"" + newName + "\", które już istnieje w bazie.";
   allNames.indexOf(newName) === -1 ? allNames.push(newName) : console.log(note);
   return allNames;
 };
 
 console.log(addName("Michał"));
-console.log();
+console.log("\n");
 console.log(addName("Misiek"));
-console.log();
+console.log("\n");
 console.log(addName("Julia"));
